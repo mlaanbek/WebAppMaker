e0228afb0b4bded436e0afc64b4eedbb4cf0048f
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-10 10:47:43 +0300

    Connect to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -3,4 +3,8 @@
 var express = require('express');
 var app = express();
 
+// connect mongoose to DB (local database named web-app-maker)
+var mongoose = require("mongoose");
+var db = mongoose.connect('mongodb://localhost/web-app-maker');
+
 app.listen(3000);
