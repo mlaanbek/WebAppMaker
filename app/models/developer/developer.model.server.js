a935f2f72c3b1e185e39cc3a602fe8517b0fb575
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-10 11:06:20 +0300

    Implement createDeveloper API

--- app/models/developer/developer.model.server.js
+++ app/models/developer/developer.model.server.js
@@ -14,6 +14,13 @@ module.exports = function (db) {
     return api;
 
     function createDeveloper(developer) {
+        // the developer object will be validated against the schema; it doesn'y have to
+        // have all the fields, but the ones that have must correspond to the schema
 
+        // object to pass and a callback function
+        // Node will not wait for this function to come back
+        Developer.create(developer, function (err, doc) {
+            console.log(doc);
+        });
     }
 };
