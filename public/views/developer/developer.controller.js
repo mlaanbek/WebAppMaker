9f2afab06b50d1ded0dadb6fa57f612307e99897
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-11 09:58:36 +0300

    Developer Service

--- public/views/developer/developer.controller.js
+++ public/views/developer/developer.controller.js
@@ -1,9 +1,21 @@
 (function () {
     angular
         .module("WebAppMakerApp")
-        .controller("DeveloperListController", developerListController);
+        // if the controllers are simple, it's ok to declare them in the same file
+        .controller("DeveloperListController", developerListController)
+        .controller("NewDeveloperController", newDeveloperController);
 
     function developerListController() {
         var vm = this;
+    }
+    
+    function newDeveloperController(DeveloperService) {
+        var vm = this;
+        vm.createDeveloper = createDeveloper;
+
+        function createDeveloper(developer) {
+            DeveloperService
+                .createDeveloper(developer);
+        }
     }
 })();
