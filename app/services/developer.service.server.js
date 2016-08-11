b304a295cc378040f8c971526b942afead35dbbc
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-11 17:35:57 +0300

    Reject promise if error

--- app/services/developer.service.server.js
+++ app/services/developer.service.server.js
@@ -11,6 +11,9 @@ module.exports = function (app, developerModel) {
             .then(
                 function (developer) {
                     res.json(developer);
+                },
+                function (err) {
+                    res.status(400).send(err);
                 }
             );
     }
