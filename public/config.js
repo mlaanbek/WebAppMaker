9cd9475c7a0b812cf5c6387c26384d3b26dbd428
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-11 09:18:38 +0300

    Declare angular application

--- public/config.js
+++ public/config.js
@@ -1,1 +1,15 @@
-
+(function () {
+    angular
+        .module("WebAppMakerApp")
+        .config(Configure);
+    
+    function Configure($routeProvider) {
+        $routeProvider
+            .when("/developer", {
+                templateUrl: "views/developer/developer-list.view.html"
+            })
+            .otherwise({
+                redirectTo: "/developer"
+            });
+    }
+})();
