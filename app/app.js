a36dddff528b972f935f26313518fd37fc9f6f51
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-14 12:07:42 +0300

    Create application web service

--- app/app.js
+++ app/app.js
@@ -5,4 +5,6 @@ module.exports = function (app, db) {
 
     // we pass "app" to enable creating URLs and developerModel to talk to the DB
     var developerService = require("./services/developer.service.server.js")(app, developerModel);
+    
+    var applicationService = require("./services/application.service.server.js")(app);
 };
