0b143c15ed5d1a18dd6ad278d9e2933fba17ef30
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-14 12:35:38 +0300

    Pass application model to service

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ module.exports = function (app, db) {
 
     // we pass "app" to enable creating URLs and developerModel to talk to the DB
     var developerService = require("./services/developer.service.server.js")(app, developerModel);
-    
-    var applicationService = require("./services/application.service.server.js")(app);
+
+    var applicationModel = require("./models/application/application.model.server")();
+    var applicationService = require("./services/application.service.server.js")(app, applicationModel);
 };
