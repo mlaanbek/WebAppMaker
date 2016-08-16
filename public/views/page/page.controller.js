8bb0ca461da30053694c3625316746f117f0c9b2
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-16 15:19:43 +0300

    Click on back button to return to list of applications

---
+++ public/views/page/page.controller.js
@@ -0,0 +1,11 @@
+(function () {
+    angular
+        .module("WebAppMakerApp")
+        .controller("PageListController", pageListController);
+
+    function pageListController($routeParams) {
+        var vm = this;
+        vm.username = $routeParams.username;
+        vm.applicationId = $routeParams.applicationId;
+    }
+})();
