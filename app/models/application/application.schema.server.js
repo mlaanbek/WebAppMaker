2b24f9443b1b0563cfc2d30ea72135346714badd
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-16 16:22:59 +0300

    Created page schema and added to application schema

--- app/models/application/application.schema.server.js
+++ app/models/application/application.schema.server.js
@@ -1,14 +1,21 @@
 var mongoose = require("mongoose");
 
 module.exports = function () {
+
+    var PageSchema = require("../page/page.schema.server.js")();
+
     var ApplicationSchema = mongoose.Schema({
         developerUsername: String,          // it's our foreign key
 
         // if many properties are defined, type must be defined explicitly
         name: {type: String, default: "Application Name"},
         description: String,
-        dateCreated: {type: Date, default: Date.now}
+        dateCreated: {type: Date, default: Date.now},
+        // basically we're saying that pages only exist inside applications, e.g composition principle
+        // (deletion of the parent also leads deletion of children)
+        // The other assumption we're making is thus that pages are not sharable.
+        pages: [PageSchema]
     }, {collection: "application"});
 
     return ApplicationSchema;
-}
+};
