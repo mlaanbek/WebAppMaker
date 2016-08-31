daaae0fcab972566bba4cf72350daeba359e9800
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-31 09:05:43 +0300

    Navigate to choose widget view when you click add widget button

--- public/views/widget/widget.controller.js
+++ public/views/widget/widget.controller.js
@@ -1,12 +1,17 @@
 (function () {
     angular
         .module("WebAppMakerApp")
-        .controller("WidgetListController", widgetListController);
+        .controller("WidgetListController", widgetListController)
+        .controller("ChooseWidgetController", chooseWidgetController);
 
     function widgetListController($routeParams, PageService, $location) {
         var vm = this;
         vm.username = $routeParams.username;
         vm.applicationId = $routeParams.applicationId;
         vm.pageId = $routeParams.pageId;
+    }
+
+    function chooseWidgetController() {
+
     }
 })();
