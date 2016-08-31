60a6a9e0210de235c2dbebd01cb2f3ebc473e0a3
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-31 09:31:59 +0300

    Created various widget schemas

---
+++ app/models/widget/label.schema.server.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+
+module.exports = function () {
+    var LabelSchema = mongoose.Schema({
+        name: String,
+        text: String
+    });
+
+    return LabelSchema;
+};
---
+++ app/models/widget/link.schema.server.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+
+module.exports = function () {
+    var LinkSchema = mongoose.Schema({
+        name: String,
+        text: String,
+        url: String,
+        pageId: String,
+        icon: String
+    });
+
+    return LinkSchema;
+};
